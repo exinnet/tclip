0ba8aded1e8e53dd980297e14542b67c79f7cea9
xingqiba <ixqbar@gmail.com>
2015-10-10 15:43:55 +0800

    update path

--- php_ext/example/test_tclip.php
+++ php_ext/example/test_tclip.php
@@ -1,9 +1,12 @@
 <?php
 
-$source_file = "../../demo_images/a1.jpg";
-$dest_file = "./a1_dest.jpg";
+$source_file = "../../demo_images/demo.jpg";
 $width = 400;
 $height = 200;
 $watermark_text = "xingqiba";
-$ret = tclip($source_file, $dest_file, $width, $height, $watermark_text); 
+
+$ret = tclip($source_file, "./a1_dest_watermark.jpg", $width, $height, $watermark_text); 
+var_dump($ret);
+
+$ret = tclip($source_file, "./a1_dest.jpg", $width, $height); 
 var_dump($ret);
